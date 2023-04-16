101dc4d1312f373b139b5678057fd672f26a1b8b
Guillaume Lopez <guillaumelopezgk@hotmail.com>
2023-04-16 18:11:11 +0900

    removed folder

--- backend/app.js
+++ backend/app.js
@@ -5,28 +5,29 @@ const bookRouter = require("./routes/books");
 const userRouter = require("./routes/users");
 const cors = require("cors");
 const path = require("path");
+require("dotenv").config();
 
-mongoose.connect(
-    "mongodb+srv://GuyLebon:Test1234@cluster0.wop2fvj.mongodb.net/grimoire",
-    {useNewUrlParser: true, useUnifiedTopology: true}
-).then(
-    () => console.log("Connexion à MongoDB réussie !")
-).catch(
-    () => console.log("Connexion à MongoDB échouée !")
-);
+mongoose
+  .connect(process.env.SECRET_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.use(express.json());
 
 app.use(cors());
 
 app.use((req, res, next) => {
-    res.set({
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-    });
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers":
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+  });
 
-    next();
+  next();
 });
 
 app.use("/api/auth", userRouter);
